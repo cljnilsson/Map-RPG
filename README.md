574f23d4ffc2d91c9f8b68e8ed0def1eb8bea569
Lukas Nilsson <zkall@outlook.com>
2025-04-26 03:55:43 +0200

    Basic creator layout

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-## Developing
+## Summery
+A childhood browser game that I always enjoyed was Travian but due to its multiplayer nature it is incredibly time demanding to play in any serious capacity as the management scales exponentially. As such I think there is room to have the spirit of Travian in a single player format. So far it is sort of a tech demo of some potential RPG aspects which will slowly be integrated together.
 
+## Key Features
+* An interactive world/city map
+* Time based upgrades / decisions, vaguely similar to an idle game
+* DnD-like character stats and story + dice
+
+## Developing
 Once you've created a project and installed dependencies with `bun install`, start a development server:
 
 ```bash
@@ -13,14 +20,14 @@ bun run dev```
 To create a production version of your app:
 
 ```bash
-npm run build
+bun run build
 ```
 
-You can preview the production build with `npm run preview`.
-
+You can preview the production build with `bun run preview`.
+s
 > To deploy your app, you may need to install an [adapter](https://svelte.dev/docs/kit/adapters) for your target environment.
 
-## New stuff
+## New stuff, personal notes
 drizzle: 
   - You will need to set DATABASE_URL in your production environment  
   - Run bun run db:push to update your database schema  
---
+++ src/routes/creator/+page.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+    let name: string;
+    let age: number;
+    let str: number, int: number, vitality: number, charisma: number, dex: number;
+    const min: number = 3, max: number = 20;
+</script>
+
+<div class="wrapper mt-5 mx-5 px-3">
+    <h3 class="text-center">Create your character!</h3>
+    <div class="row justify-content-center my-5">
+        <div class="col-6">
+            <div class="row">
+                <div class="col">
+                    <input class="form-control my-1" type="text" placeholder="Name" bind:value={name} />
+                </div>
+                <div class="col-4">
+                    <input class="form-control my-1" type="text" placeholder="Age" bind:value={age} />
+                </div>
+            </div>
+            <div class="row mt-3">
+                <div class="col">
+                    <input class="form-control my-1" type="number" placeholder="Strength" bind:value={str} min={min} max={max} />
+                    <input class="form-control my-1" type="number" placeholder="Dex" bind:value={dex} min={min} max={max} />
+                    <input class="form-control my-1" type="number" placeholder="Int" bind:value={int} min={min} max={max} />
+                </div>
+                <div class="col">
+                    <input class="form-control my-1" type="number" placeholder="Charisma" bind:value={charisma} min={min} max={max} />
+                    <input class="form-control my-1" type="number" placeholder="Vitality" bind:value={vitality} min={min} max={max} />
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="text-center">
+        <button class="btn btn-lg btn-primary">Create!</button>
+    </div>
+</div>
+
+<style>
+	.wrapper {
+		background: rgba(235, 235, 235, 0.6);
+		border-radius: 10px;
+		padding-top: 0.75rem;
+		padding-bottom: 0.75rem;
+	}
+</style>
