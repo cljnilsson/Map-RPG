034872d0bb5d8958752949795ae2cceddf4c34e7
Lukas Nilsson <zkall@outlook.com>
2025-05-02 04:43:12 +0200

    Mini menu ui for future windows and character

--- src/lib/components/map.svelte
+++ src/lib/components/map.svelte
@@ -14,9 +14,37 @@
 		/>
 		<MapClickBoxes></MapClickBoxes>
 	</div>
+	<div class="position-fixed bottom-0 start-0 m-3">
+		<div class="bg-light border rounded shadow py-2 row g-0">
+		  <div class="col px-2">
+			<img id="avatar" src="/char.jpg" alt="Your character" loading="lazy" class="img-fluid rounded" />
+			<div
+			  class="progress mt-2"
+			  role="progressbar"
+			  aria-label="Success example"
+			  aria-valuenow="90"
+			  aria-valuemin="0"
+			  aria-valuemax="100"
+			>
+			  <div class="progress-bar bg-success" style="width: 90%">90 / 100</div>
+			</div>
+		  </div>
+		  <div class="col px-2">
+			<button class="btn btn-primary btn-sm d-block mb-2">Option 1</button>
+			<button class="btn btn-secondary btn-sm d-block mb-2">Option 2</button>
+			<button class="btn btn-danger btn-sm d-block">Option 3</button>
+		  </div>
+		</div>
+	  </div>
+	  
 {/if}
 
 <style>
+	#avatar {
+		height: 100px;
+		
+	}
+
 	.map-container {
 		position: relative;
 	}
