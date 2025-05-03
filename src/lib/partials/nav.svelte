b2f0d8d397ab1e718583bfa98eba3579cb8c55e6
Lukas Nilsson <zkall@outlook.com>
2025-05-03 16:37:52 +0200

    Tiny visual improvement

--- src/lib/partials/nav.svelte
+++ src/lib/partials/nav.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { enhance } from "$app/forms";
+
 	let { data } = $props();
 
 	let isLoggedIn: boolean = $derived(data?.user);
@@ -20,8 +22,8 @@
 	<a href="/talkingtest">Dialogue demo</a>
 	<a href="/creator">Creator demo</a>
 	{#if isLoggedIn}
-		<form method="POST" action="/api/logout" style="display: inline;">
-			<button type="submit" style="background: none; border: none; color: blue; text-decoration: underline; cursor: pointer; padding: 0;">
+		<form method="POST" action="/api/logout" style="display: inline;" use:enhance>
+			<button type="submit" class="btn btn-link p-0 m-0 align-baseline">
 				Logout
 			</button>
 		</form>
@@ -31,16 +33,26 @@
 	<a href="/data">Data Visualizer</a>
 </nav>
 
-<style>
+<style lang="scss">
 	nav {
 		background: rgba(235, 235, 235, 0.6);
 		padding-top: 0.3rem;
 		padding-bottom: 0.3rem;
 	}
 
+	button {
+		color: black;
+		&:hover {
+			color: white;
+		}
+	}
+
 	a {
 		color: black;
 		padding-left: 1rem;
 		padding-right: 1rem;
+		&:hover {
+			color: white;
+		}
 	}
 </style>
