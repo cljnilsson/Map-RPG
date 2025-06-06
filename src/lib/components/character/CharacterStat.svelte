c8ec0ce376633feacf0f7aeb6e5453b78698ff4c
Lukas Nilsson <zkall@outlook.com>
2025-06-06 19:30:30 +0200

    Small readability

--- src/lib/components/character/CharacterStat.svelte
+++ src/lib/components/character/CharacterStat.svelte
@@ -6,11 +6,15 @@
         <p class="py-1">{name}</p>
     </div>
     <div class="col-2">
-        <p class="py-1" class:text-danger={value <= 5} class:text-warning={value > 5 && value <= 8} class:text-success={value >= 9}>{value}</p>
+        <p class="py-1 outlined" class:text-danger={value <= 5} class:text-warning={value > 5 && value <= 8} class:text-success={value >= 9}>{value}</p>
     </div>
 </div>
 
 <style>
+    .outlined {
+        text-shadow: 0 0 2px #000;
+    }
+
     p {
         margin-bottom: 0;
     }
