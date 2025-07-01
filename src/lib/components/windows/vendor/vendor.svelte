a8f1d9d812737e4d9e3789b5555e8198c8be6320
Lukas Nilsson <zkall@outlook.com>
2025-07-01 23:17:14 +0200

    Tooltip for vendor items.

--- src/lib/components/windows/vendor/vendor.svelte
+++ src/lib/components/windows/vendor/vendor.svelte
@@ -2,7 +2,8 @@
 	import Window from "$lib/features/window/window.svelte";
 	import WindowStore from "$lib/stores/windows.svelte";
 	import type { VendorNPC } from "$lib/types/npc";
-	import {q2c} from "$lib/utils/itemQuality";
+	import { q2c } from "$lib/utils/itemQuality";
+	import Tooltip from "$lib/features/tooltip/tooltip.svelte";
 
 	let { vendor }: { vendor: VendorNPC } = $props();
 </script>
@@ -16,15 +17,21 @@
 			{#each vendor.items as item}
 				<div class="col-6 px-1 py-1">
 					<div class="slot rounded py-1 px-1">
-						<div class="row my-1 align-items-stretch">
-							<div class="col-auto pe-1">
-								<img src={item.iconPath} alt={item.name} />
+						<Tooltip>
+							{#snippet tooltip()}
+								<h5 style={"color: " + q2c(item) + ";"}>{item.name}</h5>
+								<p>{item.description}</p>
+							{/snippet}
+							<div class="row my-1 align-items-stretch">
+								<div class="col-auto pe-1">
+									<img src={item.iconPath} alt={item.name} />
+								</div>
+								<div class="col ps-0 d-flex flex-column justify-content-between">
+									<div style={"color: " + q2c(item) + ";"}>{item.name}</div>
+									<div class="text-end mt-auto">1 2 3</div>
+								</div>
 							</div>
-							<div class="col ps-0 d-flex flex-column justify-content-between">
-								<div style={"color: " + q2c(item) + ";"}>{item.name}</div>
-								<div class="text-end mt-auto">1 2 3</div>
-							</div>
-						</div>
+						</Tooltip>
 					</div>
 				</div>
 			{:else}
@@ -37,8 +44,11 @@
 	{/snippet}
 </Window>
 
-<style>
+<style lang="scss">
 	.slot {
 		background-color: rgba(50, 50, 50, 0.9);
+		.row {
+			pointer-events: none; // fixes tooltip issues but maybe stops clicking for buying
+		}
 	}
 </style>
