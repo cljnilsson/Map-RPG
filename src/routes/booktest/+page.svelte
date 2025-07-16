bb7938125b8f5773debda6f89357d67dd164bd1b
Lukas Nilsson <zkall@outlook.com>
2025-07-16 02:55:20 +0200

    Future assets and inspiration images

--- src/routes/booktest/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-    import Book from "$lib/features/book/book.svelte";
-    import BookStore from "$lib/stores/book.svelte";
-
-	BookStore.currentBook = ["<h3>Test title</h3><p>Something here</p>", "<h3>Another page</h3><p>More content</p>"];
-</script>
-
-<Book />
