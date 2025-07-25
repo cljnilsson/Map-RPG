c9908f020b60535ada1777e6f25d46e9dcd92f31
Lukas Nilsson <zkall@outlook.com>
2025-07-25 08:31:28 +0200

    Fixed old test stuff

--- vite.config.ts
+++ vite.config.ts
@@ -7,21 +7,10 @@ const host = process.env.TAURI_DEV_HOST;
 export default defineConfig({
 	plugins: [sveltekit(), svelteTesting()],
 	clearScreen: false,
-	css: {
-		preprocessorOptions: {
-			scss: {
-				// Ensure Vite uses sass-embedded
-				implementation: require("sass-embedded")
-				// Optional: configure other sass options
-				// sourceMap: true,
-			}
-		}
-	},
 	test: {
 		environment: "jsdom",
 		setupFiles: ["./vitest-setup.js"],
-		include: ["src/**/*.{test,spec}.{js,ts}"],
-		reporters: ["hanging-process"]
+		include: ["src/**/*.{test,spec}.{js,ts}"]
 	},
 	server: {
 		port: 1420,
