6db911416bf2cb4b5824a1d6118654fd83b7e21f
Lukas Nilsson <zkall@outlook.com>
2025-07-29 13:04:17 +0200

    Type safety for saving.

--- src/lib/controller/save.svelte.ts
+++ src/lib/controller/save.svelte.ts
@@ -1,24 +1,45 @@
 import { getRequest, postRequest } from "$lib/utils/request";
 import CharacterStore from "$lib/stores/character.svelte";
 
+type CharacterInput = {
+	oldName: string,
+	name: string,
+	health: number,
+	maxHealth: number,
+	exp: number,
+	level: number,
+	stats: {
+		str: number,
+		dex: number,
+		int: number,
+		vit: number,
+		char: number
+	},
+	inventory: {
+		name: string, // Perhaps a bit missleading, might rename the 'name' attribute to key at some point
+		amount: number
+	}[]
+};
+
 export class SaveController {
-	public static async saveCharacter() {
-		await postRequest("/api/characters/save", {
-			oldName: "Alice",
-			name: "Test333333",
-			health: 25,
-			maxHealth: 100,
-			exp: 10,
-			level: 1,
+	// might rework it in the future so oldname and name are not needed but for now it is needed to have the name currently used in the database
+	public static async saveCharacter(newName: string) {
+		await postRequest<{}, CharacterInput>("/api/characters/save", {
+			oldName: CharacterStore.character.name,
+			name: newName,
+			health: CharacterStore.character.health,
+			maxHealth: CharacterStore.character.maxHealth,
+			exp: CharacterStore.character.xp,
+			level: CharacterStore.character.level,
 			stats: {
-				str: 5,
-				dex: 8,
-				int: 9,
-				vit: 10,
-				char: 5
+				str: CharacterStore.character.stats.str,
+				dex: CharacterStore.character.stats.dex,
+				int: CharacterStore.character.stats.int,
+				vit: CharacterStore.character.stats.vit,
+				char: CharacterStore.character.stats.char
 			},
 			inventory: CharacterStore.inventory.map((item) => ({
-				name: item.id, // Perhaps a bit missleading, might rename the 'name' attribute to key at some point
+				name: item.id,
 				amount: item.amount
 			})),
 		}
@@ -26,12 +47,12 @@ export class SaveController {
 	}
 
 	public static async saveWindows(newX: number, newY: number, uniqueKey: string) {
-		const resp = await postRequest("/api/characters/save/windows", {
-				key: uniqueKey,
-				x: newX,
-				y: newY,
-				characterId: CharacterStore.character.id
-			}
+		const resp = await postRequest<{ success: boolean }, { key: string, x: number, y: number, characterId: number }>("/api/characters/save/windows", {
+			key: uniqueKey,
+			x: newX,
+			y: newY,
+			characterId: CharacterStore.character.id
+		}
 		);
 
 		const { success } = resp;
