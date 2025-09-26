4671aa612c1c53c0985fe7e59a8ba6e8783c9f68
Lukas Nilsson <zkall@outlook.com>
2025-09-26 21:07:38 +0200

    latest

--- src/lib/features/window/windows/inventory/inventorySplitter.svelte
+++ src/lib/features/window/windows/inventory/inventorySplitter.svelte
@@ -9,11 +9,15 @@
 	let rightStack: number = $derived(max - current);
 
 	let inventoryWindow = WindowController.getByName("InventorySplitter");
+
+	function end() {
+
+	}
 </script>
 
 <Window
 	uniqueKey="InventorySplitter"
-	height={50}
+	height={100}
 	width={300}
 	x={inventoryWindow.x}
 	y={inventoryWindow.y}
@@ -24,15 +28,24 @@
 >
 	{#snippet title()}<h4 class="my-2">Splitter</h4>{/snippet}
 	{#snippet body()}
-		<div class="row justify-content-center align-items-center h-100">
-			<div class="col-2 text-center">
-				<span class:fw-bold={leftStack > rightStack}>{leftStack}</span>
-			</div>
-			<div class="col-auto px-0 d-flex align-items-center h-100">
-				<input type="range" class="form-range" bind:value={current} {min} {max} />
-			</div>
-			<div class="col-2 text-center">
-				<span class:fw-bold={leftStack < rightStack}>{rightStack}</span>
+		<div class="row h-100">
+			<div class="col d-flex flex-column">
+				<div class="row justify-content-center align-items-center flex-grow-1">
+					<div class="col-2 text-center">
+						<span class:fw-bold={leftStack > rightStack}>{leftStack}</span>
+					</div>
+					<div class="col-auto px-0 d-flex align-items-center h-100">
+						<input type="range" class="form-range" bind:value={current} {min} {max} />
+					</div>
+					<div class="col-2 text-center">
+						<span class:fw-bold={leftStack < rightStack}>{rightStack}</span>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col text-end">
+						<button class="btn btn-light" onclick={end}>test</button>
+					</div>
+				</div>
 			</div>
 		</div>
 	{/snippet}
