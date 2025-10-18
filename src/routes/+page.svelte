65d6abb92a56798f6c6bc0bce36fe97054ca63ff
Lukas Nilsson <zkall@outlook.com>
2025-10-18 13:57:17 +0200

    Informative first page.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,21 @@
 <script lang="ts">
-	import CharacterComponent from "$lib/components/character/Character.svelte";
+	//import CharacterComponent from "$lib/components/character/Character.svelte";
 </script>
 
 <div class="container mt-3">
-	<h1>Welcome</h1>
-	<CharacterComponent />
+	<h1>Welcome!</h1>
+	<p>More information about this project exists in the readme.md file in the base of the project folder.</p>
+	<p>In summery this is an attempt to merge two great interests of mine into a symbiotic game, namely Travian and DnD (Dungeons and Dragons).</p>
+	<p>It is noteworthy to say that I consider this game to be two two things that should be kept in mind.</p>
+	<ul>
+		<li>The game is exclusively Single Player, with maybe some mild co-op elements in the far future such as a shared marketplace.</li>
+		<li>I consider this game to be desktop first, meaning that while the game will rune fine on a mobile device the UI will not consider it to any worthwhile degree as I think displaying the amount of required information on such a small screen is not feasible without significantly holding back the potential of the desktop experience. In the far future I may consider something similar to Oldschool Runescape where their mobile client has made very significant changes that only apply on mobile.</li>
+	</ul>
+	<p>It should also be said that while this game is built using standard web technologies, it is meant to be run offline / self hosted though this is very much WIP but it is being kept in mind for a final release.</p>
+	<p>Your data / progress / save file will be easily available to the user so that it can be backed up by whatever means the user prefers.</p>
+
+	<h3>Game Info</h3>
+	A simple first-party wiki exists at <a href="/wiki">link</a> but for the most part the tutorial once implemented should tell you what you need to know.
 </div>
 
 <style>
