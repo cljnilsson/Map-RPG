e751ba2119e1ef079cc77a58fe28f4920137a96e
Lukas Nilsson <zkall@outlook.com>
2025-12-15 23:01:49 +0100

    Simplified shipyard concept.

--- src/lib/features/buildings/shipyard/shipyard.svelte
+++ src/lib/features/buildings/shipyard/shipyard.svelte
@@ -3,31 +3,15 @@
 	import Tooltip from "$lib/features/tooltip/tooltip.svelte";
 	import type { Resource } from "$lib/types/resource";
 	import { LogController } from "$lib/controller/logs.svelte";
-	import ResourceSelection from "$lib/features/buildings/market/resourceSelection.svelte";
 	import type { Building } from "$lib/types/building";
 
-	const { level, building }: {level: number, building: Omit<Building, "componentOnClick">} = $props();
-
-	let offer = $state<number | undefined>(undefined);
-	let tradeFor = $state<Resource | undefined>(undefined);
-	let tradeWant = $state<Resource | undefined>(undefined);
-
-	// Can afford if both values are set and offer is <= resource amount
-	let canAfford = $derived(tradeFor !== undefined && offer !== undefined && (tradeFor.amount ?? 0) >= offer);
-
-	let rate = $state(1.2);
-
-	function trade() {
-		if (!canAfford || !tradeFor || !tradeWant || offer === undefined) return;
-
-		const success = CityController.pay([{ ...tradeFor, amount: offer }]);
-
-		if (success) {
-			CityController.give([{ ...tradeWant, amount: Math.floor(offer * (2 - rate)) }]);
-			offer = undefined;
-			LogController.newLog("You traded resources!");
-		}
-	}
+	const { level, building }: { level: number; building: Omit<Building, "componentOnClick"> } = $props();
+	let max = $state(5);
+	let combat = $state(1);
+	let merchant = $state(2);
+	let travel = $state(1);
+	let current = $derived(combat + merchant + travel);
+	let available = $derived(max - current);
 </script>
 
 <div class="row justify-content-center align-items-center my-5">
@@ -46,51 +30,33 @@
 	{/each}
 </div>
 
-<div class="row mb-5">
-	<div class="col border">
-		<div class="row">
-			<div class="col-4 mb-4">
-				<ResourceSelection bind:selectedResource={tradeFor} />
-				<input
-					class="form-control form-control-s"
-					class:is-invalid={offer !== undefined && !canAfford}
-					type="number"
-					bind:value={offer}
-					placeholder="Amount"
-					min="1"
-				/>
-			</div>
-			<div class="col-2 border-end">
-				<div class="row">
-					<div class="col text-center">Rate</div>
-				</div>
-				<div class="row">
-					<div class="col text-center">{rate}</div>
-				</div>
-			</div>
-			<div class="col-2">
-				<div class="row">
-					<div class="col text-center">Merchants</div>
-				</div>
-				<div class="row">
-					<div class="col text-center">1</div>
-				</div>
-			</div>
-			<div class="col-4">
-				<ResourceSelection bind:selectedResource={tradeWant} />
-				{#if offer}
-					{#if tradeWant}
-						You get: <b>{Math.floor(offer * (2 - rate))}</b> {tradeWant.name}
-					{:else}
-						Pick a resource!
-					{/if}
-				{/if}
-			</div>
-		</div>
-		<div class="row my-1">
-			<div class="col text-center">
-				<button class="btn btn-primary" onclick={trade}>Trade</button>
-			</div>
-		</div>
+<div class="row justify-content-center">
+	<div class="col-4">Available</div>
+	<div class="col-2">
+		{available}
+	</div>
+</div>
+<div class="row justify-content-center">
+	<div class="col-4">Merchant ship(s)</div>
+	<div class="col-2">
+		{merchant}
+	</div>
+</div>
+<div class="row justify-content-center">
+	<div class="col-4">Travel ship(s)</div>
+	<div class="col-2">
+		{travel}
+	</div>
+</div>
+<div class="row justify-content-center">
+	<div class="col-4">Combat ship(s)</div>
+	<div class="col-2">
+		{combat}
+	</div>
+</div>
+<div class="row justify-content-center">
+	<div class="col-4">Total</div>
+	<div class="col-2">
+		{current}
 	</div>
 </div>
