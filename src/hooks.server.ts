65c3d6c973e3498aafa9dc80212b3c0e81f5ebdc
Lukas Nilsson <zkall@outlook.com>
2026-02-18 19:56:08 +0100

    Semi working reg

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,27 @@
-//import type { Handle } from "@sveltejs/kit";
-//import * as auth from "$lib/server/auth";
-//import { dev } from "$app/environment";
 import { dbCheckup } from "$lib/data/bootstrap"; // Runs health checkup for db
+import { auth } from "$lib/auth"; // path to your auth file
+import { svelteKitHandler } from "better-auth/svelte-kit";
+import { building } from "$app/environment";
+import { dev } from "$app/environment";
+import type { RequestEvent } from "@sveltejs/kit";
 
 dbCheckup();
+
+export async function handle({
+  event,
+  resolve,
+}: {
+  event: RequestEvent;
+  resolve: (event: RequestEvent) => Response | Promise<Response>;
+}): Promise<Response> {
+  if (
+    dev &&
+    event.url.pathname === "/.well-known/appspecific/com.chrome.devtools.json"
+  ) {
+    return new Response(undefined, { status: 404 });
+  }
+  return svelteKitHandler({ event, resolve, auth, building });
+}
 
 /*export const handle: Handle = async ({ event, resolve }) => {
 	// Ignore Chrome devtools probe in dev
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,21 +1,57 @@
 <script lang="ts">
-  import { enhance } from "$app/forms";
-  import type { ActionData } from "./$types";
+  import { authClient } from "$lib/auth-client";
+  import Loading from "$lib/utils/loading.svelte";
 
-  let { form }: { form: ActionData } = $props();
+  let email = $state("");
+  let password = $state("");
+  let name = $state(""); // TODO
+  let error = $state("");
+  let loading = $state(false);
+
+  async function attemptRegister() {
+    const { data } = await authClient.signUp.email(
+      {
+        email, // user email address
+        password, // user password -> min 8 characters by default
+        name, // user display name
+        callbackURL: "/", // A URL to redirect to after the user verifies their email (optional)
+      },
+      {
+        onRequest: (ctx) => {
+          loading = true;
+        },
+        onSuccess: (ctx) => {
+          loading = true;
+        },
+        onError: (ctx) => {
+          // display the error message
+          error = ctx.error.message;
+          loading = false;
+        },
+      },
+    );
+    console.log(data);
+  }
+
+  async function attemptLogin() {
+    // TODO
+  }
 </script>
 
 <div class="container mt-3">
   <div class="row justify-content-center">
     <div class="col-auto">
       <h1>Login/Register</h1>
-      <form method="post" action="?/login" use:enhance>
+      {#if loading}
+        <Loading />
+      {:else}
         <div class="mb-3">
           <label class="form-label" for="username"> Username </label>
           <input
             class="form-control text-center"
             id="username"
             name="username"
+            bind:value={email}
           />
         </div>
         <div class="mb-3">
@@ -25,16 +61,21 @@
             id="password"
             type="password"
             name="password"
+            bind:value={password}
           />
         </div>
         <div class="mt-3 text-center">
-          <button type="button" class="btn btn-primary">Login</button>
-          <button type="button" class="btn btn-primary" formaction="?/register"
-            >Register</button
+          <button type="button" class="btn btn-primary" onclick={attemptLogin}
+            >Login</button
+          >
+          <button
+            type="button"
+            class="btn btn-primary"
+            onclick={attemptRegister}>Register</button
           >
         </div>
-      </form>
-      <!-- <p style="color: red">{form?.message ?? ""}</p> -->
+        <p style="color: red">{error ?? ""}</p>
+      {/if}
     </div>
   </div>
 </div>
