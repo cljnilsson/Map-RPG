067c6bfd28f353d92b81578f4829fbba75b0ad58
Lukas <zkall@outlook.com>
2026-05-17 12:55:40 +0200

    Using tween instead, wip fix seconds timer

--- src/lib/features/tick/tick.svelte
+++ src/lib/features/tick/tick.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
     import { now } from "$lib/api/server.remote";
     import dayjs from "dayjs";
+    import { Tween } from "svelte/motion";
+
+    let test = new Tween(0, { duration: 0 });
 
     let timestamp = $state(0);
     let nextUpdate = $state(0);
     let initialRemaining = $state(0);
     let currentTime = $state(Date.now());
 
-    function tick() {
+    /*function tick() {
         currentTime = Date.now();
         requestAnimationFrame(tick);
     }
@@ -21,8 +24,7 @@
         //console.log(Math.round(100 - (remaining / initialRemaining) * 100));
 
         return Math.round(100 - (remaining / initialRemaining) * 100);
-    });
-
+        });*/
     const nextUpdateCountdown = $derived(
         Math.max(0, Math.ceil((nextUpdate - currentTime) / 1000)),
     );
@@ -32,6 +34,8 @@
             nextUpdate = result.nextUpdate;
             timestamp = result.timestamp;
             initialRemaining = result.nextUpdate - Date.now();
+            test.target = 0;
+            test.set(100, { duration: initialRemaining });
         });
     });
 </script>
@@ -40,4 +44,5 @@
     the time is {dayjs(timestamp).format("HH:mm:ss")} next update is in
     {nextUpdateCountdown} seconds
 </p>
-<progress class="w-100" max="100" value={progress}>{progress}%</progress>
+<!--<progress class="" max="100" value={progress}>{progress}%</progress>-->
+<progress class="" max="100" value={test.current}>{test.current}%</progress>
